4e24e0ef086e60f66dd3625516367dff27f0b358
Henry Ventura <hvent90@gmail.com>
2026-04-26 21:00:41 -0700

    feat(client): Interface::FindWidgetByLabel + case-insensitive doctest

--- clients/silencer/src/interface.h
+++ clients/silencer/src/interface.h
@@ -20,6 +20,12 @@ public:
 	void ActiveChanged(World & world, Interface * callinginterface, bool mouse);
 	Object * GetObjectWithUid(World & world, Uint8 uid);
 	void DestroyInterface(World & world, Interface * parentinterface = 0);
+	enum WidgetMatch { MATCH_OK, MATCH_NOT_FOUND, MATCH_AMBIGUOUS };
+	// Walks `objects`. Compares case-insensitively against widget label/text.
+	// `wantedTypes` is a bitmask of (1 << ObjectTypes::BUTTON) etc; 0 = any.
+	// Returns the matched object id in `*outId` on MATCH_OK.
+	WidgetMatch FindWidgetByLabel(class World& world, const char* labelOrId,
+		Uint32 wantedTypes, Uint16* outId) const;
 	static char * WordWrap(const char * string, unsigned int maxlength, const char * breakchar = "\n");
 	std::vector<Uint16> objects;
 	std::vector<Uint16> tabobjects;
